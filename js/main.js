bba179a6038664ebaa0e17e2e9a6aedaf88802e6
Carina Sauter <carina.sauter@berkeley.edu>
2018-02-19 11:01:21 -0800

    css changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 // Event hander for calling the SoundCloud API using the user's search query
-
 function callAPI(query) {
 	$.get("https://api.soundcloud.com/tracks?client_id=b3179c0738764e846066975c2571aebb",
 		{'q': query,
 		'limit': '200'},
 		function(data) {
-			// PUT IN YOUR CODE HERE TO PROCESS THE SOUNDCLOUD API'S RESPONSE OBJECT
-			// HINT: CREATE A SEPARATE FUNCTION AND CALL IT HERE
 			parseQuery(data);
+            // console.log(data);
 		},'json'
 	);
 }
@@ -16,7 +14,6 @@ function callAPI(query) {
 function changeTrack(url) {
 	// Remove any existing instances of the Stratus player
 	$('#stratus').remove();
-
 	// Create a new Stratus player using the clicked song's permalink URL
 	$('body').stratus({
       key: "b3179c0738764e846066975c2571aebb",
@@ -26,48 +23,56 @@ function changeTrack(url) {
     });
 }
 
-// my stuff
-
+// upon first click of submit the search result and playlist columns are created
 $(document).ready(
     $("#submitter").one('click', function() {
     	addColumns();
     }),
+    // with every click of the submission button, the Soundcloud API is queried
     $("#submitter").on('click', function() {
     	$('#searchResults').empty();
     	var user_input = $('#searchbar').val();
     	callAPI(user_input);
     }));
 
+// adding the CSS for columns into HTML
 function addColumns() {
 	$('#results').append('<div class="column" id="searchResultsContainer"><h1><b>Search Results</b></h1><div id = "searchResults"></div></div><div class="column" id="playlistContainer"><h1><b>Playlist</b></h1><div id = "playlist"></div></div>');
 }
 
+// parsing the results from the soundcloud API and injecting first 20 to HTML
 function parseQuery(data) {
 	var user_input = $('#searchbar').val();
-	for (i = 0; i < data.length; i++) {
+	for (i = 0; i < 20; i++) {
 		var cover = data[i].artwork_url;
 		var userName = data[i].user.username;
 		var playURL = data[i].permalink_url;
 		var title = data[i].title;
+        // var relatedTag = data[i].tag_list;
+        // console.log(relatedTag.split("/"));
 		var stringToAppend = '<div class = "card"><img class = "cover" src = "' + cover + '" alt= "Song Cover" onerror="imgError(this);"><div class = "information"><div class = "playURL">' + playURL + '</div><div class = "user">' + userName + '</div><div class = "title">' + title + '</div><button type="submit" class = "playSong"><div class = "buttonContainer"><img src = "img/whitePlay.png" alt="PlaySymbol">PLAY SONG</div></button><button type = "submit" class="addToPlaylist"><div class = "buttonContainer"><img src="img/whiteCross.png" alt="addIcon">ADD TO PLAYLIST</div></button></div></div>';
         $('#searchResults').append(stringToAppend);
         $('#searchbar').val('');
 	}
 }
 
+
+// replace image if cover art not found
 function imgError(image) {
     image.onerror = "";
     image.src = "img/noImgFound.png";
     return true;
 }
 
+// empty input field upon click
 $(document).on('click', 'input', function() {
     $('#searchbar').val('');
 });
 
+// adds a song to the playlist
 $(document).on('click', '.addToPlaylist', function() {
     var songToMove = $(this).parent().parent();
-    console.log(songToMove);
+    // console.log(songToMove);
 
     var playURL = $(this).parent().children()[0].innerHTML;
     var user = $(this).parent().children()[1].innerHTML;
@@ -79,6 +84,15 @@ $(document).on('click', '.addToPlaylist', function() {
 });
 
 
+// // add related songs columns
+// $(document).one('click', '.playSong', function() {
+//     console.log('hello');
+//     $("#playlistContainer").before('<div class="column" id="relatedSongsContainer"><h1><b>Related Songs</b></h1><div id = "relatedSongs"></div></div>');
+// });
+
+
+
+// plays a song
 $(document).on('click', '.playSong', function() {
     if ($(this).parent().parent().parent()[0].id == "searchResults") {
         var playURL = $(this).parent().children()[0].innerHTML;
@@ -87,9 +101,10 @@ $(document).on('click', '.playSong', function() {
         var playURL = $(this).parent().parent().children().children()[2].innerHTML;
     }
     changeTrack(playURL);
+
     });
 
-
+// shifts song one position up in the playlist
 $(document).on('click', '.up', function() {
     var thisCard = $(this).parent().parent();
     var previousSibling = $(this).parent().parent().prev();
@@ -99,6 +114,7 @@ $(document).on('click', '.up', function() {
     }
 });
 
+// shifts song one position down in the playlist
 $(document).on('click', '.down', function() {
     var thisCard = $(this).parent().parent();
     var nextSibling = $(this).parent().parent().next();
@@ -107,7 +123,7 @@ $(document).on('click', '.down', function() {
     }
 });
 
-
+// removes a song from the playlist
 $(document).on('click', '.remove', function() {
     var thisCard = $(this).parent().parent();
     thisCard.remove();
