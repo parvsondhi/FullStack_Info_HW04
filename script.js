9ec97fad5ab556c79b5b6b3386b53df13c8b1266
Bonney <bonney@berkeley.edu>
2018-02-22 17:23:15 -0800

    hw4

---
+++ script.js
@@ -0,0 +1,44 @@
+// Event hander for calling the SoundCloud API using the user's search query
+$(document).ready(function(){
+	$("#searchbutton").click(function(){
+   	var user_input = $('#song').val();
+   	callAPI(user_input);
+   });
+});
+
+
+
+
+function callAPI(query) {
+	$.get("https://api.soundcloud.com/tracks?client_id=b3179c0738764e846066975c2571aebb",
+		{'q': query,
+		'limit': '200'},
+		function(data) {
+			// PUT IN YOUR CODE HERE TO PROCESS THE SOUNDCLOUD API'S RESPONSE OBJECT
+			// HINT: CREATE A SEPARATE FUNCTION AND CALL IT HERE
+			console.log(data);
+
+			var list = $(this).parent();
+   			$("#resultssongs").prepend(list);
+		},'json'
+	);
+}
+
+
+
+// 'Play' button event handler - play the track in the Stratus player
+function changeTrack(url) {
+	// Remove any existing instances of the Stratus player
+	$('#stratus').remove();
+
+	// Create a new Stratus player using the clicked song's permalink URL
+	$.stratus({
+      key: "b3179c0738764e846066975c2571aebb",
+      auto_play: true,
+      align: "bottom",
+      links: url
+    });
+}
+
+
+
