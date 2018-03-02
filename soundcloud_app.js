45139beac935f1c92bf558331db6e44d2dd5c639
Rabberina <sipian.wang@gmail.com>
2018-03-01 23:57:15 -0800

    hw04

--- soundcloud_app.js
+++ soundcloud_app.js
@@ -5,25 +5,32 @@ $(document).ready(function(){
 var searchResults = [];
 var playlist = [];
 
+// tell the website what to do in response to user's initial actions
 function page_start() {
   $('#search-button').on('click', searchResultsButtonPressed);
+  $('#results').on('click', '.play-song', playSongButtonPressed);
+  $('#playlist').on('click', '.play-song', playSongButtonPressed);
+  $('#results').on('click', '.add-to-favorites', addToPlaylist);
+  $('#playlist').on('click', '.delete-song', deleteSongButtonPressed);
+
 }
 
+// function to pass user input to Soundcloud API
 function searchResultsButtonPressed() {
   console.log ('buttonPressed');
   //first, fetch user input
   var searchString = $('#search-input').val();
   //pass user input to callAPI function
   callAPI(searchString);
-
 }
 
+// generate results in the format of sliced array
 function saveResults(data) {
   searchResults = data.slice(0,20);
 }
 
+// format result into HTML code to be interpreted and displayed
 function generateResultHTML(result) {
-
   var id = result.id;
   var artist_name = result.user.username;
   var song_name = result.title;
@@ -31,7 +38,7 @@ function generateResultHTML(result) {
 
   if (!image_url) {
     // TODO: add placeholder image url
-    image_url = 'https://blog.hypem.com/images/artwork_unavail_bruce.png';
+    image_url = 'https://i.imgur.com/s2YUrLo.png';
   }
 
   var template = [
@@ -53,16 +60,124 @@ function generateResultHTML(result) {
   return template
 }
 
+// take the HTML code previously generated for search results, and display the search results
 function displayResults() {
-
   //delete old results, if there is any
   $('#results').empty();
   //display new results
   for (i = 0; i < searchResults.length; i++) {
        var html = generateResultHTML(searchResults[i]);
        $('#results').append(html);
+// allow users to add songs to playlist
+// not placed in page_start function as search result was not loaded then
   }
 }
+
+// play selected music
+function playSongButtonPressed() {
+  var parent = $(this).parent().parent();
+  var songId = parent.attr('id').split('-')[1];
+  var song = searchResults.find(function(element) {
+    return element.id == songId;
+  })
+
+  changeTrack(song.permalink_url);
+}
+
+// generate HTML code for playlist
+function addToPlaylist() {
+  // get the song
+  var parent = $(this).parent().parent();
+  var songId = parent.attr('id').split('-')[1];
+  var song = searchResults.find(function(element) {
+    return element.id == songId;
+  })
+
+  // allow users to oerform actions to songs on the playlist
+  // how to pass the song ID to addToPlaylist???
+  // check whether song is already in Favorites
+  for (i = 0; i < playlist.length; i++) {
+    if (song.id == playlist[i].id) {
+      return
+    }
+  }
+
+  playlist.unshift(song);
+
+  var html =  generateFavoritesHTML(song);
+  $('#playlist').prepend(html);
+
+}
+
+function generateFavoritesHTML(result) {
+  var id = result.id;
+  var artist_name = result.user.username;
+  var song_name = result.title;
+  var image_url = result.artwork_url;
+
+  if (!image_url) {
+    // TODO: add placeholder image url
+    image_url = 'https://i.imgur.com/s2YUrLo.png';
+  }
+
+// add play, delete, move up, move down buttons
+  var template = [
+    // f- for favorites, as opposed to r- for results
+    // generate the HTML template for a favorite
+    '<div id="f-', id, '"class="song-playlist" style="background-color: white; padding: 20px;">',
+      '<div class="">',
+        '<img src="', image_url,'" alt="">',
+      '</div>',
+      '<div class="">',
+        '<p> <strong>Title: </strong>', song_name, '</p>',
+        '<p> <strong>Artist: </strong>', artist_name,'</p>',
+      '</div>',
+      '<div class="">',
+        '<button class="play-song" type="button" name="button"> Play </button>',
+        '<button class="delete-song" type="button" name="button"> Delete </button>',
+        '<button class="move-up" type="button" name="button"> Move Up </button>',
+        '<button class="move-down" type="button" name="button"> Move Down </button>',
+      '</div>',
+    '</div>',
+  ].join('');
+
+  return template
+}
+
+function deleteSongButtonPressed() {
+    var parent = $(this).parent().parent();
+    var songId = parent.attr('id').split('-')[1];
+    var songIndex = playlist.findIndex(function(element) {
+      return element.id == songId;
+    });
+
+    // remove song from playlist array
+    playlist.splice(songIndex, 1);
+    //current
+    parent.remove()
+
+
+
+  // $(this).parent().remove()
+  //
+  // for (i = 0; i < playlist.length; i++) {
+  //   if (song.id == playlist[i].id) {
+  //     return
+  //   }
+  // }
+
+}
+
+function moveUp() {
+  var temp = $(this).parent();
+  elm.insertBefore(temp.prev());
+}
+
+function moveDown() {
+  var temp = $(this).parent();
+  elm.insertAfter(temp.prev());
+}
+
 
 
 function callAPI(query) {
